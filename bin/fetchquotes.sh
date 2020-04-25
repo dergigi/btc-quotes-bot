88cf3e3568a3914d07b5187d43a458b8b81ac70d
Gigi <dergigi@pm.me>
2020-04-25 13:23:17 +0200

    Make path relative to app root directory

--- bin/fetchquotes.sh
+++ bin/fetchquotes.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-curl -u $HTTP_AUTH_USERNAME:$HTTP_AUTH_PASSWORD https://www.bitcoin-quotes.com/quotes.json | python -m json.tool > ../src/quotes.json
+
+# Local: ./bin/fetchquotes.sh from application root directory.
+# Heroku: heroku run ./bin/fetchquotes.sh
+curl -u $HTTP_AUTH_USERNAME:$HTTP_AUTH_PASSWORD https://www.bitcoin-quotes.com/quotes.json | python -m json.tool > ./src/quotes.json
